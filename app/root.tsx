39b6920846ed3e0ea980bfee70330ae17e22caea
Lau Tai Hong <lautaihong2004@gmail.com>
2025-04-27 12:19:51 +0800

    clarity

--- app/root.tsx
+++ app/root.tsx
@@ -38,6 +38,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+
+        <script src="./clarity.js"></script>
       </head>
       <body>
         {children}
